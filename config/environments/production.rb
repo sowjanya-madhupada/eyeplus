73a3c57418bcfb4586bf87eae61c728a936f6387
unknown <sowjimadhupada@gmail.com>
2021-09-30 17:06:38 +0530

    third commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://secret-hamlet-53189.herokuapp.com/' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => "sowjanya2028@gmail.com",
+:password             => 'Sowji@123',
+:authentication       => "plain",
+:enable_starttls_auto => true
 end
